04be5c2b83a74ef5e476bed689e2937ca58abb45
Majdi Toumi <majdi@mhirba.com>
2021-03-09 12:23:21 +0100

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [number, updateNumber] = useState(1)
+  const [students, setStudents] = useState([])
+
+
+  const inc = () => {
+    updateNumber(number + 1)
+  }
+
+  const dec = () => {
+    updateNumber(number - 1)
+  }
+
+  const getAllStudents = async () => {
+    const response = await fetch('http://localhost:4242/api/students')
+    const students = await response.json()
+    setStudents(students)
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      getAllStudents()
+    }, 3000)
+  }, [])
+
+  const displayStudents = () => {
+    return (
+      <p>
+        There are {students.length} students :
+        {students.map((student, index) => {
+          return <span key={index} style={{ marginLeft: '10px' }}>{student.firstname}</span>
+        })}
+      </p>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {displayStudents()}
+        <p>{number}</p>
+        <button className="m-button-increment" onClick={inc}>Increment</button>
+        <button className="m-button-decrement" onClick={dec}>Decrement</button>
       </header>
-    </div>
+    </div >
   );
 }
 
